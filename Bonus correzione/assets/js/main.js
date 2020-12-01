e3e846cf8ab09f2c3cb9294c012033587a1d2c2a
Federico <federico.scarpati1989@gmail.com>
2020-12-01 19:12:27 +0100

    Esercizio con correzioni
    Esercizio rifatto dopo aver visto le correzioni in classe

---
+++ Bonus correzione/assets/js/main.js
@@ -0,0 +1,29 @@
+$(function (){
+
+
+
+var dropDown = $('li.dropdown');
+
+
+dropDown.hover(
+
+
+    function (){
+    // handler In
+
+    $(this).children('ul.m_dropdown').removeClass('d_none');
+
+
+    },
+
+    function (){
+    // handler Out
+
+    $(this).children("ul.m_dropdown").addClass("d_none");
+
+    }
+
+
+)
+
+});
